7c8fecc235d40ced287dd79f162a324ae7e58437
John Moshakis <john@moshakis.com>
2016-03-07 23:18:17 -0500

    getEmployee

--- employee-routes.js
+++ employee-routes.js
@@ -5,6 +5,39 @@ var connectionString = "postgres://JohnMoshakis@localhost:5432/JohnMoshakis";
 function getEmployee(request, response, next) {
     var id = request.params.id;
     
+    var results = [];
+    
+    pg.connect(connectionString, function(err, client, done) {
+        
+        if(err) {
+          done();
+          console.log(err);
+          response.status(500);
+          return response.json({ success: false, data: err});
+        }
+
+        var query = client.query('select id,"Name" as name from "Employees" where id = $1 ',[id], function(err, result) {
+            done();
+
+            if(err) {
+                console.log(err);
+                response.status(500);
+                return response.json({ success: false, data: err});
+            }});
+
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            done();
+            return response.json(results[0]);
+        });
+    });
+    
+    
     next();
 }
 
